9f6ef684252cef3071b768ec86153d4daba7cd88
Gerrit Larsen <nanites2000@yahoo.com>
2020-12-07 01:34:42 -0700

    day7 part 2 not working yet changed to dict and need to process from there

--- 2020/day7_2.py
+++ 2020/day7_2.py
@@ -20,24 +20,25 @@ for row in a:
         #print(bag)
         #bag_dict.update({name:[bag]})
         # the false in each entry keeps track if it has been loaded or not
-        bag_dict.setdefault(name, []).append([bag,number,False])
+        bag_dict.setdefault(name, {}).update({bag:{'number':number,'already':False}})
 print(bag_dict)
 print(bag_dict['shiny gold'])
 
 bagfinal = bag_dict['shiny gold']
 
 
-for bag in bagfinal:
-    if not bag[2]:
-        newbags = bag_dict[bag[0]]
-        print(newbags)
-        bag[2] = True
-
-        for newbag in newbags:
-            if newbag in bagfinal:
-                bagfinal[newbag][1] = bagfinal[newbag][1] + newbag[1]
-            else:
-                 bagfinal.append(bag)
-
-    print(bagfinal)
+# for bag in bagfinal:
+#     if not bag[2]:
+#         newbags = bag_dict[bag[0]]
+#         print(newbags)
+#         bag[2] = True
+#
+#         for newbag in newbags:
+#             if newbag in bagfinal:
+#                 #problem!!!!!! not  a dictionary change to one!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#                 bagfinal[newbag][1] = bagfinal[newbag][1] + newbag[1]
+#             else:
+#                  bagfinal.append(bag)
+#
+#     print(bagfinal)
 
